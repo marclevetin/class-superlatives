ae1ccf63ce5f60c793f4e3e635680af87e62be62
marclevetin <marc@derbyhat.org>
2018-03-03 18:47:20 -0500

    actually set default on model.  Added travis YML

---
+++ models/superlative.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const superlativeSchema = new Schema({
+  person: { type: String, required: [true, 'Person is required'] },
+  words: { type: String, required: [true, 'Words are required'] },
+  count: { type: Number, default: 1 }
+});
+
+const Superlative = mongoose.model("Superlative", superlativeSchema);
+
+module.exports = Superlative;
