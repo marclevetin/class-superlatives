6fbe6c944a0d78b1db1f1924b876f49db290ce5b
marclevetin <marc@derbyhat.org>
2018-03-03 18:38:18 -0500

    updated default when creating new superlative.  Deleted Book templates.

--- models/superlative.js
+++ models/superlative.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 const superlativeSchema = new Schema({
   person: { type: String, required: [true, 'Person is required'] },
   words: { type: String, required: [true, 'Words are required'] },
-  count: { type: Number, default: 0 }
+  count: { type: Number, default: 1 }
 });
 
 const Superlative = mongoose.model("Superlative", superlativeSchema);
--- routes/api/index.js
+++ routes/api/index.js
@@ -2,8 +2,6 @@ const router = require("express").Router();
 const bookRoutes = require("./books");
 const superlativeRoutes = require("./superlatives");
 
-// Book routes
-router.use("/books", bookRoutes);
 router.use("/superlatives", superlativeRoutes);
 
 module.exports = router;
