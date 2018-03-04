903ad659a5f6a02e17de0801d181e6dbcde35e52
marclevetin <marc@derbyhat.org>
2018-03-03 19:26:35 -0500

    capitalized a file name

--- models/superlative.js
+++ models/superlative.js
@@ -1,6 +1,5 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-
 const superlativeSchema = new Schema({
   person: { type: String, required: [true, 'Person is required'] },
   words: { type: String, required: [true, 'Words are required'] },
